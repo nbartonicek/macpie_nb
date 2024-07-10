49d2a1c38e5d037aab1f7e49caade53d89d482cf
nbartonicek <nbartonicek@gmail.com>
2024-07-10 12:49:19 +1000

    Added test data.

--- R/testing_read_macseq.R
+++ R/testing_read_macseq.R
@@ -1,53 +1,24 @@
+data_dir<-"inst/extdata/filtered_matrix/"
+label_file<-"inst/extdata/metadata/metadata.csv"
+gene_column = 2
+cell_column = 1
+unique_features = TRUE
+strip_suffix = FALSE
 
-#' Load in data from Mac-seq experiments
-#'
-#' This functions is a derivative from Read10X function from the Seurat package.
-#' Enables easy loading of sparse data matrices provided by 10X genomics.
-#' @author Mark Li, \email{justdont}
-#'
-#' @param data_dir Directory containing the matrix.mtx, genes.tsv
-#' (or features.tsv), and barcodes.tsv files provided by nextflow
-#' pipeline using Starsolo.
-#' Functionality for reading several data directories was removed
-#' to allow reading in the metadata label matching to the Macseq
-#' plate with minimum confusion.
-#' Next version will consider integration of other modalities.
-#'
-#' @param label_dir Directory containing the matching metadata file in csv format.
-#' @param gene_column Specify which column of genes.tsv or features.tsv
-#' to use for gene names; default is 2
-#' @param cell_column Specify which column of barcodes.tsv
-#' to use for cell names; default is 1
-#' @param unique.features Make feature names unique (default TRUE)
-#' @param strip.suffix Remove trailing "-1" if present in all cell barcodes.
-#'
-#'
-#' @return If features.csv indicates the data has multiple data types, a list
-#'   containing a sparse matrix of the data from each type will be returned.
-#'   Otherwise a sparse matrix containing the expression data will be returned.
-#'
-#' @importFrom Matrix readMM
-#' @importFrom utils read.delim
-#'
-#' @export
-#' @concept preprocessing
-#'
-#' @examples
-#' \dontrun{
-#' #
+
 #' data_dir <- 'path/to/data/directory'
 #' list.files(data_dir) # Should show barcodes.tsv, genes.tsv, and matrix.mtx
 #' expression_matrix <- Readmacseq(data_dir = data_dir)
 #' macpie_object = CreateMacPie(counts = expression_matrix)
 #' }
 #'
-readMacSeq <- function(
-  data_dir,
-  label_dir,
-  gene_column = 2,
-  cell_column = 1,
-  unique.features = TRUE,
-  strip.suffix = FALSE
+read_macseq <- function(
+    data_dir,
+    label_file,
+    gene_column = 2,
+    cell_column = 1,
+    unique_features = TRUE,
+    strip_suffix = FALSE
 ) {
 
   full_data <- list()
@@ -59,48 +30,39 @@ readMacSeq <- function(
   if (!dir.exists(paths = run)) {
     stop("Macseq data directory provided does not exist")
   }
-  barcode_loc <- file.path(run, "barcodes.tsv")
+  barcode_loc <- file.path(run, "barcodes.tsv.gz")
   features_loc <- file.path(run, "features.tsv.gz")
-  matrix_loc <- file.path(run, "matrix.mtx")
+  matrix_loc <- file.path(run, "matrix.mtx.gz")
 
-  if (!dir.exists(paths = metadata)) {
-    stop("Metadata directory provided does not exist")
+  if (!file.exists(label_file)) {
+    stop("Metadata file provided does not exist")
   }
-  label_loc <- list.files(metadata, pattern = "\\.csv$", full.names = TRUE)
+  label_file <- list.files(metadata, pattern = "\\.csv$", full.names = TRUE)
 
-  if (!file.exists(barcode_loc)) {
-    stop("Barcode file missing. Expecting ",
-         basename(path = barcode_loc))
+  if (!any(file.exists(c(barcode_loc,features_loc,matrix_loc)))) {
+    stop(paste(
+      "Missing one or more of count matrix files. Expecting",
+      basename(path = barcode_loc),
+      basename(path = features_loc),
+      basename(path = matrix_loc))
+    )
   }
-  if (!file.exists(features_loc)) {
-    stop("Features file missing. Expecting ",
-         basename(path = features_loc))
-  }
-  if (!file.exists(matrix_loc)) {
-    stop("Expression matrix file missing. Expecting ",
-         basename(path = matrix_loc))
-  }
-  if (!file.exists(label_loc)) {
-    stop("Metadata file missing. Expecting ", basename(path = label_loc))
+
+  if (!file.exists(label_file)) {
+    stop("Metadata file missing. Expecting ", basename(path = label_file))
   }
   warning("Make sure you only have 1 metadata file in the folder.")
 
   data <- readMM(file = matrix_loc)
-  if (has_dt) {
-    cell_barcodes <- as.data.frame(
-      data.table::fread(barcode_loc, header = FALSE)
-    )
-  } else {
-    cell_barcodes <- read.table(file = barcode_loc,
-      header = FALSE, sep = "\t", row.names = NULL)
-  }
+  cell_barcodes <- as.data.frame(data.table::fread(barcode_loc, header = FALSE))
+
 
   if (ncol(x = cell_barcodes) > 1) {
     cell_names <- cell_barcodes[, cell_column]
   } else {
     cell_names <- readLines(con = barcode_loc)
   }
-  if (all(grepl(pattern = "\\-1$", x = cell_names)) && strip.suffix) {
+  if (all(grepl(pattern = "\\-1$", x = cell_names)) && strip_suffix) {
     cell_names <- as.vector(x = as.character(x = sapply(
       X = cell_names,
       FUN = Seurat::ExtractField,
@@ -119,8 +81,8 @@ readMacSeq <- function(
   }
 
   if (has_dt) {
-    feature_names <- as.data.frame(data.table::fread(
-      ifelse(pre_ver_3, gene_loc, features_loc), header = FALSE)
+    feature_names <- as.data.frame(
+      data.table::fread(ifelse(pre_ver_3, gene_loc, features_loc), header = FALSE)
     )
   } else {
     feature_names <- read.delim(
@@ -137,17 +99,23 @@ readMacSeq <- function(
       call. = FALSE,
       immediate. = TRUE
     )
-    na.features <- which(x = is.na(x = feature_names[, gene_column]))
-    replacement_column <- ifelse(test = gene_column == 2, yes = 1, no = 2)
-    feature_names[na.features, gene_column] <-
-      feature_names[na.features, replacement_column]
+    na_features <- which(is.na(feature_names[, gene_column]))
+    replacement_column <- ifelse(gene_column == 2, yes = 1, no = 2)
+    feature_names[na_features, gene_column] <-
+      feature_names[na_features, replacement_column]
   }
-  if (unique.features) {
+  if (unique_features) {
     fcols <- ncol(x = feature_names)
     if (fcols < gene_column) {
-      stop(paste0("gene_column was set to ", gene_column,
-        " but feature.tsv.gz (or genes.tsv) only has ", fcols, " columns.",
-        " Try setting the gene_column argument to a value <= to ", fcols, "."))
+      stop(
+        paste0(
+          "gene_column was set to ",
+          gene_column,
+          " but feature.tsv.gz (or genes.tsv) only has ",
+          fcols,
+          " columns. Try setting the gene_column argument to a value <= to ",
+          fcols, ".")
+      )
     }
     rownames(x = data) <- make.unique(names = feature_names[, gene_column])
   }
